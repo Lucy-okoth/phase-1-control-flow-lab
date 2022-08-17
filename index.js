4672f4eef39f1d9d394884261e6b4b2fe894ff49
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-08-17 21:46:40 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+if (ride <= 400){
+  return 'This one is on me!'
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+else if (ride > 2000 && ride<=2500){
+  return 'I will gladly take your thirty bucks.'
 }
-
-function switchOnCharmFromTip(){
+else return 'No can do.'
+}
   // Write your code here!
+  scuberGreetingForFeet(2501)
+  function ternaryCheckCity(city){
+    return (city === 'NYC'? 'Ok, sounds good.':'No go.')
+  }
+  console.log(ternaryCheckCity('NYC'));
+
+ 
+ /*
+ function scuberGreetingForFeet(distance){
+  let charges;
+ }
+ */
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+      case 'not as generous':
+      return 'Thank you.';
+      default:
+        return 'Bye.';
+  }
 }
